b31733eb7e73a98fc6c966bdfbf7c9f85a03e55d
brettahn <abu89@naver.com>
2025-11-20 08:11:02 +0900

    [Part1] Ch 6,7 : Copy-on-write & Deep copy(MegaMartV3)

---
+++ src/main/kotlin/com/study/groking/part1/ch4/MegaMartV3.kt
@@ -0,0 +1,126 @@
+package com.study.groking.part1.ch4
+
+class MegaMartV3 {
+
+    private var shoppingItem = listOf<Item>() // Action
+//    private var shoppingCartTotal: Int = 0 // Action
+
+    // Action
+    fun addItemToCart(name: String, price: Int) {
+        shoppingItem = addItem(shoppingItem, Item(name, price))
+
+        val total = calcTotal(shoppingItem)
+        setCartTotalDom()
+        updateShippingIcons(shoppingItem)
+        updateTaxDom(total)
+    }
+
+    fun deleteHandler(name: String) {
+        shoppingItem = removeItemByName(shoppingItem, name)
+
+        val total = calcTotal(shoppingItem)
+        setCartTotalDom()
+        updateShippingIcons(shoppingItem)
+        updateTaxDom(total)
+    }
+
+    // Action
+    fun updateShippingIcons(cart: List<Item>) {
+        val buyButtons = getBuyButtonsDom()
+        for (button in buyButtons) {
+            val item = button.item
+            val newCarts = addItem(cart, item)
+            if (getFreeShipping(newCarts)) {
+                button.showFreeShoppingIcon()
+            } else {
+                button.hideFreeShippingIcon()
+            }
+        }
+    }
+
+    fun removeItemByName(cart: List<Item>, name: String): List<Item> {
+        var idx: Int? = null
+        for ((i, item) in cart.withIndex()) {
+            if (item.name == name) {
+                idx = i
+                break
+            }
+        }
+
+        if (idx != null) {
+            return removeItem(cart, idx)
+        }
+        return cart
+    }
+
+    fun removeItem(array: List<Item>, index: Int): List<Item> {
+        val newArray = array.toMutableList()
+        newArray.removeAt(index)
+        return newArray
+    }
+
+    // Action
+    fun updateTaxDom(total: Int) {
+        // 세금 반영을 위한 DOM 업데이트
+        setTaxDom(calcTax(total))
+    }
+
+    private fun setCartTotalDom() {
+        // 금액 합계 반영을 위한 DOM 업데이트
+    }
+
+    private fun getBuyButtonsDom(): List<BuyButton> {
+        // 모든 구매버튼의 정보
+        return listOf(
+            BuyButton(Item("item1", 1000))
+        )
+    }
+
+    // Calculation
+    private fun addItem(cart: List<Item>, item: Item): List<Item> {
+        return addElementLast(cart, item)
+    }
+
+    // Calculation
+    private fun addElementLast(array: List<Item>, elem: Item): MutableList<Item> {
+        val newArray = array.toMutableList()
+        newArray.add(elem)
+        return newArray
+    }
+
+    // Calculation
+    private fun calcTotal(cart: List<Item>): Int {
+        var total = 0
+        for (item in cart) {
+            total += item.price
+        }
+        return total
+    }
+
+    // Calculation
+    private fun getFreeShipping(cart: List<Item>) = calcTotal(cart) >= 20
+
+    // Calculation
+    private fun calcTax(amount: Int) = amount * 0.1
+
+    private fun setTaxDom(d: Double) {
+        TODO("Not yet implemented")
+    }
+
+    data class Item(
+        val name: String,
+        val price: Int
+    );
+
+    data class BuyButton(
+        val item: Item
+    ) {
+        fun showFreeShoppingIcon() {
+            // 무료 배송 아이콘 표시
+        }
+
+        fun hideFreeShippingIcon() {
+            // 무료 배송 아이콘 숨김
+        }
+    };
+}
